70aaf609305b8c403c7d6cda16270e5fcead0679
Mark <farao.development@gmail.com>
2020-02-01 21:42:50 +0100

    audio on interaction

--- GLAZE_AVB/Assets/Scripts/Coins.cs
+++ GLAZE_AVB/Assets/Scripts/Coins.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class Coins : MonoBehaviour
 {
     [SerializeField]int value;
-    [Range(1f,100f)][SerializeField]float rotationSpeed=5f;
+    [Range(0f,100f)][SerializeField]float rotationSpeed=5f;
+    [SerializeField] AudioClip coinClip;
     GameManager gmc;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,7 @@ public class Coins : MonoBehaviour
     }
     private void OnMouseDown()
     {
+        AudioSource.PlayClipAtPoint(coinClip, transform.position);
         gmc.addCoin(value);
         Destroy(this.gameObject);
     }
--- GLAZE_AVB/Assets/Scripts/CoreBehavior.cs
+++ GLAZE_AVB/Assets/Scripts/CoreBehavior.cs
@@ -1,22 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class HealthDisplay : MonoBehaviour
+public class CoreBehavior : MonoBehaviour
 {
-    Text text;
-    GameManager gmc;
     // Start is called before the first frame update
     void Start()
     {
-        text = GetComponent<Text>();
-        gmc = FindObjectOfType<GameManager>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        text.text = gmc.getHealth().ToString();
+        
     }
 }
