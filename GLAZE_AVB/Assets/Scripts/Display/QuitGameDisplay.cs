49f3582ba2ff23ea659ec63544888b087786fa76
MionoraD <machdalena1@gmail.com>
2020-02-02 14:22:06 +0100

    Add quit function

---
+++ GLAZE_AVB/Assets/Scripts/Display/QuitGameDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGameDisplay : MonoBehaviour
+{
+    Button btnQuit;
+    GameManager gmc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnQuit = gameObject.GetComponent<Button>();
+        btnQuit.onClick.AddListener(QuitGame);
+    }
+
+    public void QuitGame()
+    {
+        // save any game data here
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+    }
+}
